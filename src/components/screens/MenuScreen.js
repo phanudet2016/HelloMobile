d73fa80945b2fc9c6e324786d81c101b39cbe944
phanudet2016 <panudach_beer_2012@hotmail.co.th>
2019-01-02 01:31:37 +0700

    1/2/2019

--- src/components/screens/MenuScreen.js
+++ src/components/screens/MenuScreen.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
-import { Text, View} from 'react-native';
+import { Text, View, Button, PermissionsAndroid  } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import SendIntentAndroid from 'react-native-send-intent'
 
 import Header from '../Header/HeaderMenu'
 
 export default class MenuScreen extends Component {
 
+    _makePhoneCall = async () => {
+        try {
+            const granted = await PermissionsAndroid.request (
+                PermissionsAndroid.PERMISSIONS.CALL_PHONE
+                // , {
+                //     'title': 'App Call Phone Permission',
+                //     'message': 'App needs access to your call phone feature'
+                // }
+              )
+              if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                  SendIntentAndroid.sendPhoneCall('*121%23', true);
+              } else {
+                  console.log("Call Phone permission denied")
+              }
+        } catch (err) {
+            console.warn(err)
+        }
+    }
+    
     static navigationOptions = ({ navigation }) => {
         return {
           tabBarLabel: 'เมนู',
@@ -14,11 +34,18 @@ export default class MenuScreen extends Component {
           )
         }
     };
+    
 
     render() {
         return (
             <View>
                 <Header/>
+                <View>
+                    <Button
+                    onPress={() => this._makePhoneCall()}
+                    title='Check'
+                    />
+                </View>
             </View>
         );
     }
